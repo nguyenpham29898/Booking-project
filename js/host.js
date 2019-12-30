4156effa0885619516699c6af5776034b5817db9
= <=>
2019-12-30 14:53:38 +0700

    Update css

--- js/host.js
+++ js/host.js
@@ -7,6 +7,14 @@ $(function() {
     $(".new-dialog-wrapper").removeClass("active");
     return false;
   });
+  $("#sua").click(function() {
+    $(".edit_promotion").addClass("live");
+    return false;
+  });
+  $(".wrapper_header_btn_back").click(function() {
+    $(".edit_promotion").removeClass("live");
+    return false;
+  });
 });
 $(function() {
   $(".menu-user").click(function() {
